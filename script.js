415e02647c69009f19553b736baf0106cbf2eab2
mrush336 <mrush336@gmail.com>
2020-03-27 20:13:11 -0400

    refactoring on branch revision

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-//Book constructor function
-function Book(title, author, pages, read = Boolean){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  
-  }
-
-//Add a book to the library
+// Instansiate the library array
 const myLibrary = [];
-function addBook(){
-  const formTitle = document.getElementById('title').value;
-  const formAuthor = document.getElementById('author').value;
-  const formPages = document.getElementById('pages').value;
-  const formRead = document.getElementById('read').checked;
- 
-  let sample = new Book(formTitle, formAuthor, formPages, formRead);
-  myLibrary.push(sample);
- 
-  generateTable(table, myLibrary);
+
+//Book constructor function
+function Book(id, title, author, pages, read = Boolean) {
+	this.id = id;
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	this.info = function() {
+		return ` Book Title: "${title}", Author: "${author}", Pages: ${pages} `;
+	};
 }
 
-//Display books on screen
-const table = document.querySelector('tbody');
-function generateTable(table, data) {
-  let rowCount = data.length;
-  for (let i=0; i < rowCount; i++){
-  table.deleteRow(-1);
-  }
-  for (let element of data) {
-    let row = table.insertRow();
-    for (key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      cell.appendChild(text);
-    }
-  }
+//Add a book to the library
+function addBook() {
+	let savedName = document.getElementById("saveAs").value;
+	let formTitle = document.getElementById("title").value;
+	let formAuthor = document.getElementById("author").value;
+	let formPages = document.getElementById("pages").value;
+	let formRead = document.getElementById("read").checked;
+
+	savedName = new Book(savedName, formTitle, formAuthor, formPages, formRead);
+	myLibrary.push(savedName);
+	console.log(myLibrary);
+	//console.log(savedName);
+	displayBook(savedName);
+}
+
+// Create and add the elements on the page
+function displayBook(book) {
+	let card = document.createElement("div");
+	card.setAttribute("id", `${book.id}`);
+	let cardText = document.createElement("h2");
+	let cardContent = document.createTextNode(book.info());
+	card.appendChild(cardText).appendChild(cardContent);
+
+	let bookBtn = document.createElement("button");
+	let buttonText = document.createTextNode("DELETE");
+	bookBtn.setAttribute("onclick", "deleteBook(this)");
+	bookBtn.appendChild(buttonText);
+
+	let buttonContainer = document.createElement("div");
+	buttonContainer.appendChild(bookBtn);
+
+	let container = document.getElementById("output");
+	container.appendChild(card).appendChild(buttonContainer);
+}
+
+function deleteBook(button) {
+	console.log("book deleted");
+	document.getElementById(button.parentNode.parentNode.id).remove();
 }
--- snippets.js
+++ snippets.js
@@ -1,27 +1,40 @@
-
 let table = document.querySelector("table");
 generateTableHead(table);
 
 function generateTableHead(table, data) {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key of data) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
+	let thead = table.createTHead();
+	let row = thead.insertRow();
+	for (let key of data) {
+		let th = document.createElement("th");
+		let text = document.createTextNode(key);
+		th.appendChild(text);
+		row.appendChild(th);
+	}
 }
 let table = document.querySelector("table");
 let data = Object.keys(mountains[0]);
 generateTableHead(table, data);
 
+Book.prototype.info = function() {
+	return `${title}, written by ${author}, ${pages} pages, has been read? ${read}`;
+};
 
-Book.prototype.info = function(){
-  return (`${title}, written by ${author}, ${pages} pages, has been read? ${read}`);
+let storedLibrary = JSON.stringify(sample, null);
+localStorage.setItem(`${saveAs}`, storedLibrary);
+
+//Display books on screen
+const table = document.querySelector("tbody");
+function generateTable(table, data) {
+	let rowCount = data.length;
+	for (let i = 0; i < rowCount; i++) {
+		table.deleteRow(-1);
+	}
+	for (let element of data) {
+		let row = table.insertRow();
+		for (key in element) {
+			let cell = row.insertCell();
+			let text = document.createTextNode(element[key]);
+			cell.appendChild(text);
+		}
+	}
 }
-
- let storedLibrary = JSON.stringify(sample, null,);
-  localStorage.setItem(`${saveAs}`, storedLibrary);
-
-  
